30b5e1c09e35057aa2dea00e910867096abf4604
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,11 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+End Class
